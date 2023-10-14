91b133b54f76769e335ee3713643d4fcfe57bf17
SANTHOSH17-DOT <santhoshm12052003@gmail.com>
2023-10-14 12:59:10 +0530

    Add: try catch block Handle no path case in the add command

--- src/vc/init.ts
+++ src/vc/init.ts
@@ -1,23 +1,28 @@
 import fs from "fs";
 export async function Init(cwd: string,ipfs_node_url:string){
-    let reinitialize = false;
-    if(fs.existsSync(cwd+"/.statik")){
-        reinitialize = true;
-        fs.rmSync(cwd+"/.statik", {recursive: true})
+    try{
+        let reinitialize = false;
+        if(fs.existsSync(cwd+"/.statik")){
+            reinitialize = true;
+            fs.rmSync(cwd+"/.statik", {recursive: true})
+        }
+        fs.mkdirSync(cwd+"/.statik")
+        fs.mkdirSync(cwd+"/.statik/refs/heads",{recursive: true})
+    
+        fs.writeFileSync(cwd+"/.statik/HEAD", "")
+        fs.writeFileSync(cwd+"/.statik/SNAPSHOT", "")
+        fs.writeFileSync(cwd+"/.statik/CONFIG", JSON.stringify({
+            ipfs_node_url: ipfs_node_url
+        }))
+    
+        if(reinitialize){
+            console.error("Reinitialized Statik repository in "+cwd+"/.statik")
+        }else{
+            console.log("Initialized Statik repository in "+cwd+"/.statik")
+        }
+        process.exit(0)
+    }catch(e){
+        console.error(e)
+        process.exit(1)
     }
-    fs.mkdirSync(cwd+"/.statik")
-    fs.mkdirSync(cwd+"/.statik/refs/heads",{recursive: true})
-
-    fs.writeFileSync(cwd+"/.statik/HEAD", "")
-    fs.writeFileSync(cwd+"/.statik/SNAPSHOT", "")
-    fs.writeFileSync(cwd+"/.statik/CONFIG", JSON.stringify({
-        ipfs_node_url: ipfs_node_url
-    }))
-
-    if(reinitialize){
-        console.error("Reinitialized Statik repository in "+cwd+"/.statik")
-    }else{
-        console.log("Initialized Statik repository in "+cwd+"/.statik")
-    }
-    process.exit(0)
 }
--- src/vc/stage.ts
+++ src/vc/stage.ts
@@ -2,56 +2,65 @@ import { create, globSource } from "ipfs-http-client";
 import { IsStatik } from "../utils/checkStatik.js";
 import fs from 'fs'
 import { FetchConfig } from "../utils/fetchConfig.js";
-// If order of elements changes, the hash changes
-// Fix the logic (Sort maybe?)
-// Accept file path '.'
 export async function Add(cwd:string,paths:string[]){
-    IsStatik(cwd)
-    const client = create({url: FetchConfig(cwd).ipfs_node_url})
-    const prevCommit = fs.readFileSync(cwd+"/.statik/HEAD").toString()
-    if(!prevCommit.length){
-        let snapshot=[];
-        for (const path of paths){
-            for await (const result of client.addAll(globSource(path,{recursive:true}))) {
-                snapshot.push(result)
-            }
+    try{
+        IsStatik(cwd)
+        if(!paths.length){
+            console.log("No file path specified!")
+            console.log("Hint: statik help")
+            return
         }
-        // console.log(snapshot)
-        const result = await client.add(JSON.stringify(snapshot))
-        fs.writeFileSync(cwd+"/.statik/SNAPSHOT",result.path)
-        console.log(
-            "Files staged to IPFS with cid: "+result.path
-        )
-    }else{
-        let asyncitr = client.cat(prevCommit)
-        let prevSnapshot = "";
-        for await(const itr of asyncitr){
-            const data = Buffer.from(itr).toString()
-            prevSnapshot = JSON.parse(data).snapshot
-        }
-        let prevContent = [];
-        asyncitr = client.cat(prevSnapshot)
-        for await(const itr of asyncitr){
-            const data = Buffer.from(itr).toString()
-            prevContent = JSON.parse(data)
-        }
-        // Not optimized
-        for (const path of paths){
-            for await (const result of client.addAll(globSource(path,{recursive:true}))) {
-                for(const prev of prevContent){
-                    if(prev.path==result.path){
-                        prevContent.splice(prevContent.indexOf(prev),1)
-                        break;
-                    }
+        const client = create({url: FetchConfig(cwd).ipfs_node_url})
+        const prevCommit = fs.readFileSync(cwd+"/.statik/HEAD").toString()
+        if(!prevCommit.length){
+            let snapshot=[];
+            for (const path of paths){
+                for await (const result of client.addAll(globSource(path,{recursive:true}))) {
+                    snapshot.push(result)
                 }
-                prevContent.push(result)
             }
+            // console.log(snapshot)
+            const result = await client.add(JSON.stringify(snapshot))
+            fs.writeFileSync(cwd+"/.statik/SNAPSHOT",result.path)
+            console.log(
+                "Files staged to IPFS with cid: "+result.path
+            )
+        }else{
+            let asyncitr = client.cat(prevCommit)
+            let prevSnapshot = "";
+            for await(const itr of asyncitr){
+                const data = Buffer.from(itr).toString()
+                prevSnapshot = JSON.parse(data).snapshot
+            }
+            let prevContent = [];
+            asyncitr = client.cat(prevSnapshot)
+            for await(const itr of asyncitr){
+                const data = Buffer.from(itr).toString()
+                prevContent = JSON.parse(data)
+            }
+            // Not optimized
+            for (const path of paths){
+                for await (const result of client.addAll(globSource(path,{recursive:true}))) {
+                    let flag = true
+                    for(const prev of prevContent){
+                        if(prev.path==result.path){
+                            prevContent.splice(prevContent.indexOf(prev),1,result)
+                            flag = false
+                            break;
+                        }
+                    }
+                    if(flag) prevContent.push(result)
+                }
+            }
+            const result = await client.add(JSON.stringify(prevContent))
+            fs.writeFileSync(cwd+"/.statik/SNAPSHOT",result.path)
+            console.log(
+                "Files staged to IPFS with cid: "+result.path
+            )
         }
-        const result = await client.add(JSON.stringify(prevContent))
-        fs.writeFileSync(cwd+"/.statik/SNAPSHOT",result.path)
-        console.log(
-            "Files staged to IPFS with cid: "+result.path
-        )
+        process.exit(0)
+    }catch(e){
+        console.error(e)
+        process.exit(1)
     }
-    process.exit(0)
 }
