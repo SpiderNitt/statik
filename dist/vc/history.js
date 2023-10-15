3a63003b707815f091f8b35b19d1a5d936edfe30
SANTHOSH17-DOT <santhoshm12052003@gmail.com>
2023-10-15 22:47:51 +0530

    Update build

--- dist/vc/history.js
+++ dist/vc/history.js
@@ -3,19 +3,25 @@ import { IsStatik } from "../utils/checkStatik.js";
 import fs from 'fs';
 import { FetchConfig } from "../utils/fetchConfig.js";
 export async function Log(cwd) {
-    IsStatik(cwd);
-    const client = create({ url: FetchConfig(cwd).ipfs_node_url });
-    let head = fs.readFileSync(cwd + "/.statik/HEAD").toString();
-    let asyncitr = client.cat(head);
-    console.log("Commit history: ");
-    while (head.length) {
-        for await (const itr of asyncitr) {
-            const data = Buffer.from(itr).toString();
-            console.log("<" + head + ">" + " " + JSON.parse(data).message);
-            head = JSON.parse(data).prevCommit;
-            asyncitr = client.cat(head);
+    try {
+        IsStatik(cwd);
+        const client = create({ url: FetchConfig(cwd).ipfs_node_url });
+        let head = fs.readFileSync(cwd + "/.statik/HEAD").toString();
+        let asyncitr = client.cat(head);
+        console.log("Commit history: ");
+        while (head.length) {
+            for await (const itr of asyncitr) {
+                const data = Buffer.from(itr).toString();
+                console.log("<" + head + ">" + " " + JSON.parse(data).message);
+                head = JSON.parse(data).prevCommit;
+                asyncitr = client.cat(head);
+            }
         }
+        process.exit(0);
     }
-    process.exit(0);
+    catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
 }
 //# sourceMappingURL=history.js.map
--- dist/vc/init.js
+++ dist/vc/init.js
@@ -1,23 +1,29 @@
 import fs from "fs";
 export async function Init(cwd, ipfs_node_url) {
-    let reinitialize = false;
-    if (fs.existsSync(cwd + "/.statik")) {
-        reinitialize = true;
-        fs.rmSync(cwd + "/.statik", { recursive: true });
+    try {
+        let reinitialize = false;
+        if (fs.existsSync(cwd + "/.statik")) {
+            reinitialize = true;
+            fs.rmSync(cwd + "/.statik", { recursive: true });
+        }
+        fs.mkdirSync(cwd + "/.statik");
+        fs.mkdirSync(cwd + "/.statik/refs/heads", { recursive: true });
+        fs.writeFileSync(cwd + "/.statik/HEAD", "");
+        fs.writeFileSync(cwd + "/.statik/SNAPSHOT", "");
+        fs.writeFileSync(cwd + "/.statik/CONFIG", JSON.stringify({
+            ipfs_node_url: ipfs_node_url
+        }));
+        if (reinitialize) {
+            console.error("Reinitialized Statik repository in " + cwd + "/.statik");
+        }
+        else {
+            console.log("Initialized Statik repository in " + cwd + "/.statik");
+        }
+        process.exit(0);
     }
-    fs.mkdirSync(cwd + "/.statik");
-    fs.mkdirSync(cwd + "/.statik/refs/heads", { recursive: true });
-    fs.writeFileSync(cwd + "/.statik/HEAD", "");
-    fs.writeFileSync(cwd + "/.statik/SNAPSHOT", "");
-    fs.writeFileSync(cwd + "/.statik/CONFIG", JSON.stringify({
-        ipfs_node_url: ipfs_node_url
-    }));
-    if (reinitialize) {
-        console.error("Reinitialized Statik repository in " + cwd + "/.statik");
+    catch (e) {
+        console.error(e);
+        process.exit(1);
     }
-    else {
-        console.log("Initialized Statik repository in " + cwd + "/.statik");
-    }
-    process.exit(0);
 }
 //# sourceMappingURL=init.js.map
